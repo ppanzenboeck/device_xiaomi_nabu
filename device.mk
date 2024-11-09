9150a5f0f66de34868a133633d6bc111008966cd
ppanzenboeck <panz.development@gmail.com>
2024-11-09 12:38:55 +0100

    adapt lineage to derpfest

--- device.mk
+++ device.mk
@@ -16,12 +16,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Conditionally inherit GMS makefiles
-ifneq ("$(wildcard vendor/gapps/arm64/arm64-vendor.mk)", "")
-$(warning Gapps directory was found, They'll be bundled in.)
-$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
-endif
-
 ifneq ("$(wildcard packages/apps/ViPER4AndroidFX/config.mk)", "")
 $(warning Enabling Viper4Android audio effect.)
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
